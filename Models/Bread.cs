92e9c2ee48651b04a2b155a84e85f47860ff2460
Alex Skreen <skreenalex@gmail.com>
2020-02-28 12:27:29 -0800

    add if/else statement to HowManyPastries

--- Models/Bread.cs
+++ Models/Bread.cs
@@ -22,32 +22,28 @@ namespace Bakery.Bread
     string stringHowManyLoaves = Console.ReadLine();
     Console.WriteLine("You want " + stringHowManyLoaves + " loaves?");
     int howManyLoaves = int.Parse(stringHowManyLoaves);
-    
-      // if (userBread.BreadCost <= 2)
-      // {
-      // userBread.BreadCost = howManyLoaves * 5;
-      // Console.WriteLine(userBread.BreadCost);
-      // }
-      if (howManyLoaves %3 == 0)
+    userBread.Loaves = (howManyLoaves);    
+      if (userBread.Loaves %3 == 0)
       {
-      int freeLoaves = (howManyLoaves/3);
-      userBread.BreadCost = ((howManyLoaves - freeLoaves) * 5);
+      int freeLoaves = (userBread.Loaves/3);
+      userBread.BreadCost = ((userBread.Loaves - freeLoaves) * 5);
       Console.WriteLine(userBread.BreadCost);
+      Console.WriteLine("userBread.BreadCost: " + userBread.BreadCost);
+      Console.WriteLine("userBread.Loaves: " + userBread.Loaves);
       }
       else
       {
-      int freeLoaves = (howManyLoaves/3);
-      userBread.BreadCost = ((howManyLoaves - freeLoaves) * 5);
-      Console.WriteLine(userBread.BreadCost);
+      int freeLoaves = (userBread.Loaves/3);
+      userBread.BreadCost = ((userBread.Loaves - freeLoaves) * 5);
+      Console.WriteLine("userBread.BreadCost: " + userBread.BreadCost);
+      Console.WriteLine("userBread.Loaves: " + userBread.Loaves);
+
       }
     }
     else
     {
       Console.WriteLine("That's too bad, our bread is really good!");
     }
-
-
-
     }
   }
   
@@ -61,6 +57,40 @@ namespace Bakery.Bread
       PastryCost = pastryCost;
       Pastries = pastries;
 
+    }
+    public static void HowManyPastries(Pastry userPastry)
+    {
+    Console.WriteLine("Would you like any pastries today?");
+    string userPastryResponse = Console.ReadLine();
+
+    if (userPastryResponse.Contains("yeah") || userPastryResponse.Contains("sure") || userPastryResponse.Contains("yep") || userPastryResponse.Contains("yes"))
+    {
+    Console.WriteLine("How many pastries would you like?");
+    string stringHowManyPastries = Console.ReadLine();
+    Console.WriteLine("You want " + stringHowManyPastries + " pastries?");
+    int howManyPastries = int.Parse(stringHowManyPastries);
+    userPastry.Pastries = (howManyPastries);    
+      if (userPastry.Pastries %3 == 0)
+      {
+      int freePastries = (userPastry.Pastries/3);
+      userPastry.PastryCost = ((userPastry.Pastries * 2) - freePastries);
+      Console.WriteLine(userPastry.PastryCost);
+      Console.WriteLine("userPastry.PastryCost: " + userPastry.PastryCost);
+      Console.WriteLine("userPastry.Pastries: " + userPastry.Pastries);
+      }
+      else
+      {
+      int freePastries = (userPastry.Pastries/3);
+      userPastry.PastryCost = ((userPastry.Pastries * 2) - freePastries);
+      Console.WriteLine("userPastry.PastryCost: " + userPastry.PastryCost);
+      Console.WriteLine("userPastry.Pastries: " + userPastry.Pastries);
+
+      }
+    }
+    else
+    {
+      Console.WriteLine("That's too bad, our Pastry is really good!");
+    }
     }
   }
 }
